2bcef1daa246ce78d6dbe8e8052ca9dda5fcd05e
Conor Ahern <35696968+conora@users.noreply.github.com>
2019-11-13 20:43:00 -0500

    Delay motor updates less often

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -17,42 +17,47 @@
  */
 void opcontrol() {
 	logging::clearLogFile();
-	//pros::lcd::set_text(1, "Hi, Shane!");
-	//pros::lcd::print(2, "Before if statement");
+	int count = 0;
+
 	while (true) {
-		if(Hardware::controller.get_digital_new_press(DIGITAL_X))
-		{
-			//Hardware::left_drive.moveAbsolute(5, 100);
-			while(!Hardware::drive_system.drive_forward(12, .5))
-			{
-				pros::delay(20);
-			}
+        // Only update motors every 2ms
+	    if(!(count % 2)) {
+            if (Hardware::controller.get_digital_new_press(DIGITAL_X)) {
+                //Hardware::left_drive.moveAbsolute(5, 100);
+                while (!Hardware::drive_system.drive_forward(12, .5)) {
+                    pros::delay(20);
+                }
+            }
+
+            int left = Hardware::controller.get_analog(ANALOG_LEFT_Y);
+            int right = Hardware::controller.get_analog(ANALOG_RIGHT_Y);
+
+            Hardware::drive_system.drive(left, right);
+
+            if (Hardware::controller.get_digital(DIGITAL_R2)) {
+                Hardware::lift.raise(200);
+            } else if (Hardware::controller.get_digital(DIGITAL_R1)) {
+                Hardware::lift.lower(200);
+            } else /*if(Hardware::lift.isMoving())*/{
+                Hardware::lift.stop();
+            }
+
+            Hardware::horiz_intake.run_intake(Hardware::controller.get_digital(DIGITAL_A),
+                                              Hardware::controller.get_digital(DIGITAL_B));
+        }
+
+	    // Log motors every 25ms
+	    if(!(count % 25)) {
+            //Log all motors
+            //Hardware::drive_system.logDrive();
+            Hardware::lift.logLift();
+        }
+
+		pros::delay(1);
+		count++;
+
+		if (count > 2000) {
+		    count = 1;
 		}
-
-		int left = Hardware::controller.get_analog(ANALOG_LEFT_Y);
-		int right = Hardware::controller.get_analog(ANALOG_RIGHT_Y);
-
-		Hardware::drive_system.drive(left, right);
-
-		if(Hardware::controller.get_digital(DIGITAL_R2)){
-			Hardware::lift.raise(200);
-		}
-		else if(Hardware::controller.get_digital(DIGITAL_R1)){
-			Hardware::lift.lower(200);
-		}
-		else /*if(Hardware::lift.isMoving())*/{
-			Hardware::lift.stop();
-		}
-
-		Hardware::drive_system.drive(left, right);
-
-		Hardware::horiz_intake.run_intake(Hardware::controller.get_digital(DIGITAL_A), Hardware::controller.get_digital(DIGITAL_B));
-
-		//Log all motors
-		//Hardware::drive_system.logDrive();
-		Hardware::lift.logLift();
-
-
-		pros::delay(20);
 	}
 }
