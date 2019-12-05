f281c2e23681efa557db65bedebf6e59ff5c9d2e
superrm11 <ramtech51@comcast.net>
2019-12-05 18:55:48 -0500

    First auto partially programmed

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,13 +1,25 @@
 #include "main.h"
+#include "hardware.h"
+
+enum AUTO_COLOR
+{
+    BLUE = 1,
+    RED = -1
+};
 
 enum AUTO_1_STATE
 {
-    INIT, DRIVE1, COLLECT_AO, TURN1, DRIVE2, TURN2, DRIVE3, COLLECT_Q,
-    REVERSE1, TURN3, DRIVE4, DROP1, REVERSE_AWAY, END
+    INIT, DRIVE1, COLLECT_AO, TURN1, DRIVE2, TURN2, COLLECT_Q,
+    REVERSE1, TURN3, DRIVE3, DROP1, REVERSE_AWAY, END
 };
 
 
 AUTO_1_STATE auto_1_current = INIT; 
+AUTO_COLOR color = BLUE;
+
+float drive_speed = .5;
+float drive_slow_speed = .2;
+float turn_speed = .5; 
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -30,44 +42,69 @@ void autonomous()
             case INIT:
             // Inititalize / calibrate period
 
+
+            auto_1_current = DRIVE1;
             break;
             case DRIVE1:
             // Drive forward towards cube group A(red) or O(blue)
+
+            if(Hardware::drive_system.drive_forward(5, drive_speed))
+                auto_1_current = COLLECT_AO;
 
             break;
             case COLLECT_AO:
             // Collect the cubes, one at a time, driving forward slowly
 
+            Hardware::horiz_intake.run_intake(true, false);
+
+            if(Hardware::drive_system.drive_forward(drive_slow_speed, 22))
+                auto_1_current = TURN1;
+
             break;
             case TURN1:
             // Turn towards the Q group of cubes
+            Hardware::horiz_intake.run_intake(false, false);
+            if(Hardware::drive_system.turn_degrees(color * 60, turn_speed))
+                auto_1_current = DRIVE2;
 
             break;
             case DRIVE2:
             // Drive to complete a lateral translation towards Q
 
+            if(Hardware::drive_system.drive_forward(11.5, drive_speed))
+                auto_1_current = TURN2;
+
             break;
             case TURN2:
             // Turn again to complete the 2 point turn
 
-            break;
-            case DRIVE3:
-            // Drive towards the Q group
+            if(Hardware::drive_system.turn_degrees(color * -60, turn_speed))
+                auto_1_current = COLLECT_Q;
 
             break;
             case COLLECT_Q:
             // Collect the single Q cube
 
+            Hardware::horiz_intake.run_intake(true, false);
+            if(Hardware::drive_system.drive_forward(3, drive_slow_speed))
+                auto_1_current = REVERSE1;
+
             break;
             case REVERSE1:
             // Begin reversing to drop off cubes
+
+            if(Hardware::drive_system.drive_forward(-32, drive_speed))
+                auto_1_current = TURN3;
 
             break;
             case TURN3:
             // Turn towards scoring zone
 
+            if(Hardware::drive_system.turn_degrees(color * -135, turn_speed))
+                auto_1_current = DRIVE3;
+
             break;
-            case DRIVE4:
+            case DRIVE3:
             // Drive to scoring zone
 
             break;
